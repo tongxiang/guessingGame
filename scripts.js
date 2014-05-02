316ecac20a1c82fa37b63d2121310f6f106d9323
Tong Xiang <tong.xiang2@gmail.com>
2014-05-01 20:12:30 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Project Requirements:
+
+// When a game begins, there should be a random number generated between 1-100.
+// The user should have an input field where they can submit a guess.
+// After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
+// Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
+// Validate inputs to test if they are real numbers between 1-100.
+// Create a new game button that resets the game.
+// Store all of the guesses and create a way to check if the guess is a repeat.
+// Create a button that provides the answer (Give me a Hint).
+// Submit the guess by pressing enter or clicking the submit button.
+// Change the background color, add an image, or do something creative when the user guesses the correct answer.
+
+var answerStore = function(){
+     
+}
+
+var initiateGame = function(){
+
+}
